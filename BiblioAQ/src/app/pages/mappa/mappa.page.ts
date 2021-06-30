389896f81417c6521388b17f177bf2d8895a48ac
Luca-te <luca.marinelli@student.univaq.it>
2021-06-30 23:17:38 +0200

    map

--- BiblioAQ/src/app/pages/mappa/mappa.page.ts
+++ BiblioAQ/src/app/pages/mappa/mappa.page.ts
@@ -1,103 +1,111 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {NavController, Platform} from '@ionic/angular';
-//import {GoogleMaps, GoogleMapsEvent, LatLng} from '@ionic-native/google-maps';
-
-
-declare let google;
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {Geolocation, Geoposition} from '@ionic-native/geolocation/ngx';
+declare var google;
 
 @Component({
   selector: 'app-mappa',
   templateUrl: './mappa.page.html',
   styleUrls: ['./mappa.page.scss'],
 })
-export class MappaPage implements OnInit {
+export class MappaPage implements AfterViewInit {
 
- @ViewChild('map') mapElement: ElementRef;
+  map;
+  @ViewChild('mapElement', {static: false}) mapElement;
 
-  //map: any;
- //mapInitialised: boolean = false;
-  //apiKey: any;
+  mapOptions = {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+  };
+  address;
+  cityName;
+  stateName;
+  constructor(private geolocation: Geolocation) {
+    
 
-  constructor() {
-    //this.loadGoogleMaps();
+   }
+
+  loadMap(){
+   this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+    const marker=new google.maps.Marker({
+      position: this.mapOptions.center,
+      map:this.map,
+      title: "Locazione corrente",
+    });
+    this.geocodeLatLng(this.mapOptions.center);
+
   }
-/*
-  loadGoogleMaps(){
+  geocodeLatLng(currentPosition) {
+    const geocoder = new google.maps.Geocoder();
+    
+    geocoder.geocode({ location: currentPosition },(results, status) => {
+        if (status==='OK') {
+          if(results[0]){
+            console.log(results);
+            this.address=results[0].formatted_address;
+            if(results[0].address_components.length >0){
 
-    this.addConnectivityListeners();
+              results[0].address_components.array.forEach(item => {
+                if(item.types.indexOf('locality')!== -1){
+                  this.cityName=item.short_name;
+                }
+                if(item.types.indexOf('"administrative_area_level_1"')!== -1){
+                  this.stateName=item.short_name;
+                }
+                
+              });
+            }
 
-    if(typeof google == 'undefined' || typeof google.maps == 'undefined'){
-
-      console.log('Google maps JavaScript needs to be loaded.');
-      this.disableMap();
-
-      if(this.connectivityService.isOnline()){
-        console.log('online, loading map');
 
 
-        const script = document.createElement('script');
-        script.id = 'googleMaps';
 
-        if(this.apiKey){
-          script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+            this.map.setZoom(11);
+            const marker = new google.maps.Marker({
+            position: currentPosition,
+            map: this.map,
+          });
         } else {
-          script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+          window.alert("No results found");
         }
-
-        document.body.appendChild(script);
-
+      }else {
+        window.alert("Geodecoder failed due to: "+status);
       }
-    }
-    else {
-
-      if(this.connectivityService.isOnline()){
-        console.log('showing map');
-
-        this.enableMap();
-      }
-      else {
-        console.log('disabling map');
-        this.disableMap();
-      }
-
+      });
     }
 
-  }
 
 
-  disableMap(){
-    console.log('disable map');
-  }
 
-  enableMap(){
-    console.log('enable map');
-  }
 
-  addConnectivityListeners(){
 
-    const onOnline = () => {
 
-      setTimeout(() => {
-        if(typeof google == 'undefined' || typeof google.maps == 'undefined'){
 
-          this.loadGoogleMaps();
 
-        }
-        this.enableMap();
-      }, 2000);
 
-    };
 
-    const onOffline = () => {
-      this.disableMap();
-    };
 
-    document.addEventListener('online', onOnline, false);
-    document.addEventListener('offline', onOffline, false);
 
-  }
-*/
-  ngOnInit(): void {
+
+
+
+
+
+  ngAfterViewInit(): void {
+
+
+    this.geolocation.getCurrentPosition().then((resp:Geoposition) => {
+      this.mapOptions.center.lat=resp.coords.latitude;
+      this.mapOptions.center.lng=resp.coords.longitude;
+      this.loadMap();
+    }).catch((error) => {
+      console.log('Error getting location',error);
+    });
+
+
+
+
+
+
+    
   }
 
 }

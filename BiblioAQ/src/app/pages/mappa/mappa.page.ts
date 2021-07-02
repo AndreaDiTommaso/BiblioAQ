645c90b144959b0a852276d0baae8e5ecac95421
Luca-te <luca.marinelli@student.univaq.it>
2021-07-02 15:40:09 +0200

    Mappa aggiornata

--- BiblioAQ/src/app/pages/mappa/mappa.page.ts
+++ BiblioAQ/src/app/pages/mappa/mappa.page.ts
@@ -1,24 +1,118 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {Geolocation, Geoposition} from '@ionic-native/geolocation/ngx';
-declare let google;
+import {ElementRef} from '@angular/core';
+
+declare var google:any;
 
 @Component({
   selector: 'app-mappa',
   templateUrl: './mappa.page.html',
   styleUrls: ['./mappa.page.scss'],
 })
-export class MappaPage implements AfterViewInit {
+export class MappaPage {
 
-  map;
-  @ViewChild('mapElement', {static: false}) mapElement;
+  map :any;
+  @ViewChild('map', {read:ElementRef,static: false}) mapRef:ElementRef;
 
-  mapOptions = {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8,
-  };
-  address;
-  cityName;
-  stateName;
+  infoWindows:any=[];
+  markers:any=[
+    {
+      title: "National Art Gallery",
+      latitude: "-17.824991",
+      longitude:"31.049295"
+    },
+    {
+      title: "Dominican Convent School",
+      latitude: "-17.822647",
+      longitude:"31.052042"
+    },
+    {
+      title: "West End Hospital",
+      latitude: "-17.820987",
+      longitude:"31.039682"
+    }
+
+  ];
+
+  constructor() {}
+
+  ionViewDidEnter()
+  {
+    this.showMap();
+  }
+
+
+  addMarkersToMap(markers){
+    for(let marker of markers){
+      let position=new google.maps.LatLng(marker.latitude,marker.longitude);
+      let mapMarker=new google.maps.Marker({
+        position:position,
+        title:marker.title,
+        latitude:marker.latitude,
+        longitude:marker.longitude
+
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+
+  }
+
+  addInfoWindowToMarker(marker){
+    let infoWindowContent= '<div id="content">'+
+                              '<h2 id ="firstHeading" class"firstHeading">'+marker.title+'</h2>'+
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitide: ' + marker.longitude +'</p>'+
+                              '</div>';
+  let infoWindow=new google.maps.infoWindow({
+    content: infoWindowContent
+  });
+
+  marker.addListener('click',()=>{
+    this.closeAllInfoWindows();
+    infoWindow.open(this.map, marker);
+  });
+  this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows(){
+    for(let window of this.infoWindows){
+      window.close();
+    }
+  }
+
+
+
+
+
+
+
+  showMap(){
+    const location=new google.maps.LatLng(-17.824858,31.053028);
+    const options={
+      center:location,
+      zoom:15,
+      disableDefaultUI:true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers);
+  }
+
+
+
+
+
+
+
+//  mapOptions = {
+ //   center: {lat: -34.397, lng: 150.644},
+ //   zoom: 8,
+ // };
+ //address;
+  //cityName;
+  //stateName;
+/*
   constructor(private geolocation: Geolocation) {
 
 
@@ -73,22 +167,6 @@ export class MappaPage implements AfterViewInit {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   ngAfterViewInit(): void {
 
 
@@ -106,6 +184,6 @@ export class MappaPage implements AfterViewInit {
 
 
 
-  }
+  }*/
 
 }
